b572459ce0b435e6025875adbc8047243a921a48
RuthlessPuppet <97858010+RuthlessPuppet@users.noreply.github.com>
2023-04-27 13:25:14 -0700

    Should register now?

--- DiscordAPI.js
+++ DiscordAPI.js
@@ -36,4 +36,25 @@ class ScratchDiscord {
                 ],
         };
     }
+
+    fetchURL({url}) {
+        return fetch(url).then(response => response.text())
+    }
+    
+    jsonExtract({name,data}) {
+        var parsed = JSON.parse(data)
+        if (name in parsed) {
+            var out = parsed[name]
+            var t = typeof(out)
+            if (t == "string" || t == "number")
+                return out
+            if (t == "boolean")
+                return t ? 1 : 0
+            return JSON.stringify(out)
+        }
+        else {
+            return ""
+        }
+    }
 }
+Scratch.extensions.register(new ScratchDiscord());
